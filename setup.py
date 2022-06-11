ab44719d2a3f6b22ebe7797f63b567a5d614bdcb
WasinUddy <wasinsilakong@gmail.com>
2022-06-11 18:01:01 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'corgidb',         # How you named your package folder (MyLib)
-  packages = ['corgidb'],   # Chose the same as "name"
+  packages = ['corgidb', 'corgidb.objects'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Basic sqlite3 wrapper write as short as a corgi legs and as fast as a corgi',   # Give a short description about your library
   author = 'Wasin Silakong',                   # Type in your name
   author_email = 'wasin.silakong@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/WasinUddy/CorgiDB',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/WasinUddy/CorgiDB/archive/refs/tags/v0.1.0-alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/WasinUddy/CorgiDB/archive/refs/tags/v0.2.0-alpha.tar.gz',    # I explain this later on
   keywords = ['sql python', 'python database', 'SQL', 'python sqlite'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
