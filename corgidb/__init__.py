0bc6792ee57fa551cb86e7674472b7951a9ee2a5
WasinUddy <wasinsilakong@gmail.com>
2022-06-11 18:05:41 +0700

    v0.1.3

--- corgidb/__init__.py
+++ corgidb/__init__.py
@@ -2,5 +2,5 @@ from multiprocessing import Condition
 from corgidb.corgidb import CorgiDB
 from corgidb.utils import Utils
 from corgidb.objects.condition import Condition
-from corgidb.objects.datachunkq import DataChunk
+from corgidb.objects.datachunk import DataChunk
 from corgidb.objects.table import Table
